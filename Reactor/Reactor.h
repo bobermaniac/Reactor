550b1af9f70b634d4b2c5b99857b94f39eaf17e2
Victor Bryksin <bober_maniac@virtualmind.ru>
2017-07-13 22:34:40 +0300

    Initial Commit

---
+++ Reactor/Reactor.h
@@ -0,0 +1,19 @@
+//
+//  Reactor.h
+//  Reactor
+//
+//  Created by Victor Bryksin on 13/07/2017.
+//  Copyright © 2017 Victor Bryksin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reactor.
+FOUNDATION_EXPORT double ReactorVersionNumber;
+
+//! Project version string for Reactor.
+FOUNDATION_EXPORT const unsigned char ReactorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reactor/PublicHeader.h>
+
+
